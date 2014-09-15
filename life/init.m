87da8f0bd455ca08f83f05a80dbe20fc3531c081
Simon D. Levy <simon.d.levy@gmail.com>
2014-09-15 14:44:57 -0400

    Create init.m

---
+++ life/init.m
@@ -0,0 +1,21 @@
+function grid = init(n)
+% Initializes an NxN grid.
+% Called automatically by CA program; do not call from command-line.
+% You modify this for each CA application.
+%
+% Copyright (C) 2009  Simon D. Levy
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+
+% grid is initially empty, allowing user to click-in values
+grid = zeros(n);
+
+
