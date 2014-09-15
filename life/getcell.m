8518c89feee540d80c68f19331c5f00cbb557175
Simon D. Levy <simon.d.levy@gmail.com>
2014-09-15 14:45:23 -0400

    Create getcell.m

---
+++ life/getcell.m
@@ -0,0 +1,20 @@
+function newcell = getcell
+% Responds to left mousebutton click on grid.
+% Called automatically by CA program; do not call from command-line.
+% You modify this for each CA application.
+%
+% Copyright (C) 2009  Simon D. Levy
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+
+
+% left click turns cell on
+newcell = 1;
